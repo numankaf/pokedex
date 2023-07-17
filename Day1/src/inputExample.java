b06d640bbbbf6c2798bf53c7c641d10a903ad836
Numan Kafadar <numan.kafadar@globetech.io>
2023-07-17 15:06:49 +0300

    input double  example

---
+++ Day1/src/inputExample.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class inputExample {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a double : ");
+        double d = scanner.nextDouble();
+        System.out.printf("Double value you entered is %f \n",d );
+    }
+}
