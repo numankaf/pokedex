90b47eed6bae21d009f406da6f4383f53e752d81
Numan Kafadar <numan.kafadar@globetech.io>
2023-08-10 19:29:10 +0300

    routing protection added

--- pokedex-frontend/src/app/admin/page.jsx
+++
@@ -1,7 +0,0 @@
-const AdminPage  = () =>{
-    return (
-        <h1>This is admin page</h1>
-    )
-}
-
-export default AdminPage;
---
+++ pokedex-frontend/src/middleware.js
@@ -0,0 +1,27 @@
+import {NextResponse} from "next/server";
+import {getTokenDecoded} from "@/utils/getTokenDecoded";
+import {protectedRoutes, publicRoutes} from "@/utils/routes";
+
+
+export function middleware(request) {
+    const currentUser = request.cookies.get("currentUser");
+    let user;
+    if(currentUser){
+         user = JSON.parse(currentUser.value);
+    }
+
+    // console.log(request.nextUrl.pathname)
+    if (!currentUser &&(request.nextUrl.pathname.includes('/main/') || request.nextUrl.pathname.includes('/admin/'))) {
+        return NextResponse.redirect("http://localhost:3000/auth/login")
+    }
+
+    if(user && user.role ==="TRAINER" && request.nextUrl.pathname.includes('/admin/')){
+        return NextResponse.redirect("http://localhost:3000/404");
+    }
+
+    if(user && user.role ==="ADMIN" && request.nextUrl.pathname.includes('/main/')){
+        return NextResponse.redirect("http://localhost:3000/404");
+    }
+
+
+}
