b5bcd76c71e35b7ec5fcbb39f3306ef72795e18c
Numan Kafadar <numankafadar97@gmail.com>
2023-08-18 17:13:36 +0300

    api gateway added

--- pokedex-mikroservices/api-gateway/src/main/java/com/pokedex/apigateway/ApiGatewayApplication.java
+++ pokedex-mikroservices/api-gateway/src/main/java/com/pokedex/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.pokedex.pokemonservice;
+package com.pokedex.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PokemonServiceApplication {
-
+public class ApiGatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PokemonServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayApplication.class, args);
     }
-
 }
